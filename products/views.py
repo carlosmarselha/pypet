801c3beea37680f78278fa273bf804ea94215fcb
Carlos Marselha <carlos.marselha@sysser.com.br>
2025-09-13 13:14:00 -0300

    head

--- products/views.py
+++ products/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # escreve 
 def product_view(request):
-    return render(request, 'product_list.html', {})
+    return render(request, 'pages/product_list.html', {})
